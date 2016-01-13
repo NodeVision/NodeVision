a49d1bb54222ec8984ca9af3c863d088077a48ce
Valentin <valentin.arshanes.thomas@gmail.com>
2016-01-13 09:20:36 +0100

    Créations connexionUI

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+/// <reference path="lib/express.d.ts" />
+/// <reference path="lib/serve-static.d.ts" />
+/// <reference path="lib/mime.d.ts" />
+/// <reference path="lib/socketIO.d.ts" />
+
+import * as express from "express";
+import * as sio from "socket.io";
+import * as http from "http";
+
+class Server {
+    private app = express();
+    private httpServer = http.createServer(this.app);
+    private io = sio.listen(this.httpServer);
+
+    constructor() {
+        //routage sur index.html
+        this.app.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
+        });
+        //ajout des dépendences
+        this.app.use(express.static(__dirname + '/'));
+        //connexion & deconnexion
+        this.io.on('connection', function (socket: SocketIO.Socket) {
+            console.log('a user connected');
+            socket.on('disconnect', function () {
+                console.log('user disconnected');
+            });
+        });
+        //montage du server
+        this.httpServer.listen(52310, function () {
+            console.log('listening on *:52310');
+        });
+    }
+}
+new Server();
