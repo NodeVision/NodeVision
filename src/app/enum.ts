afa2862372a2ada32193469fdc1fc5d0b8f1bc85
Thomas Laumondais <thomas.laumondais@gmail.com>
2016-01-25 09:06:18 +0100

    rm all

--- src/app/enum.ts
+++
@@ -1,4 +0,0 @@
-﻿export enum Action { create, read, update, delete }
-export enum Status { sucess, warning, fail }
-export enum Element { graph, branch, node, edge, attribut, user }
-export enum EDGE_TYPE{ hierarchy, custom }
