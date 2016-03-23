9beefa38a72d6f0ea3a9dbeccfbea0bd07ba6205
benjamin troquereau <troquereaub@gmail.com>
2016-03-23 14:02:46 +0100

    correction de la requete de base

--- src/app/bellmanford.ts
+++ src/app/bellmanford.ts
@@ -1,7 +1,6 @@
 import {Graph} from './model/graph';
-
 export class Bellmanford{
-    //function qui afficher le resultat
+    //function qui affiche le resultat
     public printArray(dist:number[],num:number){
         
         console.log("Vertex   Distance from Source\n")
@@ -9,41 +8,32 @@ export class Bellmanford{
             console.log("%d \t\t %d\n", i, dist[i])
         }
     }
-
-    //implementation de algorithme bellmanford
-    //qui chercher les distance minimal de source à chaque points dans le gragh
-    //ce function aussi detecte les cycle de "negative weight"
-
-    public BellmanFord(graph :Graph ,depart:number){
+    
+    public BellmanFord(graph :Graph ,depart:number,type_weight:string){
         var V=graph.nodes.length;
         var E=graph.edges.length;
         var dist=new Array<number>();
-        //initialiser les distances entre source et chaque point avec un valeur infini 
+        //initialiser les distances entre la source et chaque point avec une valeur infini 
         for(var i=0;i<=V;i++){
             dist[i]=Number.MAX_VALUE;
         }
-        
         dist[depart]=0;
-        
-        //Détender tous les edges | V | - 1 fois. 
-        //Un chemin simple et plus court à partir de src à tout autre sommet peut avoir au plus | V | - 1 arêtes
+        //détection des poids positif
         for(var i=0; i<=V-1; i++){
             for(var j=0; j<E; j++){
                 var src=graph.edges[j].source.id;
-                var dest=graph.edges[j].target.id;
-                var weight = graph.edges[j].weight;
+                var dest=graph.edges[j].target.id;               
+                var weight = graph.edges[j].weight.find(x => x.name == type_weight).value;
                 if( dist[src]+weight<dist[dest] ){
                     dist[dest]=dist[src]+weight;
                 }
             }
         }
-        //verfier les cycles"negative-weight"
-        //ce boucle garantit que les plus courtes distances ne contient pas de cycle"negative-weight"
-        //si nous obtenons un chemin encore plus court que le resultat de ce boucle alors il a contient au mois un cycle "negative-weight"
+        //détection des poids négatif
         for(var j=0; j<E; j++){
                 var src=graph.edges[j].source.id;
                 var dest=graph.edges[j].target.id;
-                var weight = graph.edges[j].weight;
+                var weight = graph.edges[j].weight.find(x => x.name == type_weight).value;
                 if( dist[src]+weight<dist[dest] ){
                     console.log("Graph contains negative weight cycle");
                 }
--- src/app/graphUI.ts
+++ src/app/graphUI.ts
@@ -74,8 +74,6 @@ export class GraphUI {
         this.bdd();
         /////////////////////TEST NOTIFICATION 
         var n = new Notification(this.user,'test',new Branch('lol'));
-        //console.log(this.user);
-        
         this.notifications.push(n,n) 
         ///////////////////////
         //navbar branches
@@ -196,7 +194,7 @@ export class GraphUI {
         /**/ });
         /**/ // Del edge broadcast
         /**/ this.socket.on('del edge clt', (edge) => {
-                    console.log(edge);
+                    
         /**/      // del to graph 
         /**/      var toSplice = this.graph.edges.filter((l) => { return (l.source.id === edge.source._id) && (l.target.id === edge.target._id); });
         /**/      toSplice.map((l) => { this.graph.edges.splice(this.graph.edges.indexOf(l), 1); });
@@ -510,7 +508,7 @@ export class GraphUI {
 
     public show_profile()
     {
-        console.log(this.user.preferedView)
+       
         this.preferedView0 = this.user.preferedView == 0;
         this.preferedView1 = this.user.preferedView == 1;
         this.usermodalstate = true;
@@ -524,7 +522,7 @@ export class GraphUI {
     }
     /** This is a description of the  function. */
     public delete_branch(branch: Branch) {
-        console.log(branch);
+       
         this.socket.emit('del branch srv', branch);
     }
     
@@ -548,10 +546,10 @@ export class GraphUI {
     }
     /** This is a description of the  function. */
     public update_edge(edgename:string) {
-        console.log(edgename);
+        
         this.edge.name = edgename;
         this.title_state = false;
-        console.log(this.edge);
+      
         this.socket.emit('up edge srv', this.edge);
     }
     /** This is a description of the  function. */
@@ -619,7 +617,7 @@ export class GraphUI {
         if (auth_user.length == 0){
            auth_user =this.query(Action.create,new User(mail,0,null,null,null,null,this.authentication.getPicture()))
         }
-       console.log(auth_user);
+       
        
         //hydrate le user
         this.user = new User(
@@ -636,63 +634,54 @@ export class GraphUI {
         this.users_authentified.push(this.user);
         
         //Récupération de tous les noeuds sur lesquels on a la vision
+        var reqNode = "MATCH (u:User)-[ru:KNOWS|WRITE]->(n:Node)<-[re:BELONG]-(b:Branch) WHERE id(u)="+this.user.node.id+" RETURN n,b";
+        var reqEdge = "MATCH (u:User)-->()-[r:HIERARCHICAL|CUSTOM]-() WHERE id(u)="+this.user.node.id+" RETURN r";
+     
+        var responseNode = this.query(Action.read,null,reqNode);
+        var responseEdge = this.query(Action.read,null,reqEdge);
 
-        var req = "MATCH (u:User)-[ru:KNOWS|WRITE|READ|CUSTOM]->(n:Node)-[r:HIERARCHICAL]-()<-[re:BELONG]-(b:Branch) WHERE id(u) = "+this.user.id+" RETURN keys(n),n,r,b";
-        var response = this.query(Action.read,null,req)
-        console.log(response);
         //Récupération de tous les utilisateurs qui ne sont pas nous même
         var reponse_users = this.query(Action.read,null,"MATCH (u:User) WHERE id(u) <> "+this.user.node.id+" RETURN u");
         this.graph = new Graph(1, 'graph');         
-        reponse_users.forEach(u => {   
-                    console.log("img "+u[0].data.image_path);
-            var n = new NVNode(
-                        this.userBranch,
-                        u[0].metadata.id,
-                        u[0].data.mail,
-                        [new Attribute('name',u[0].data.name),
-                        new Attribute('firstname',u[0].data.firstname)],null,
-                        u[0].data.image_path,
-                        u[0].metadata.labels[0])
-     
-            this.users.push(new User(u[0].data.mail,u[0].data.preferedView,u[0].metadata.id,n));
-            this.graph.nodes.push(n);
+        reponse_users.forEach(u => {               
+            this.users.push(new User(u[0].data.mail,u[0].data.preferedView,u[0].metadata.id));
          });
        
-        response.forEach(n => { // par chaque noeud
+        responseNode.forEach(n => { // par chaque noeud
                 this.listAttribute = new Array<Attribute>();
-                n[0].forEach(nameAttribut => {
+                for(var index in n[0].data) {
+                    var nameAttribut = n[0].data[index];
                     if(nameAttribut != "name")
-                        {
-                            var att = new Attribute(nameAttribut,n[1].data[nameAttribut])
-                            this.listAttribute.push(att);
-                        }
-                }); 
-
-                if(!this.found(this.graph.nodes,n[1].metadata.id)){
+                    {
+                        var att = new Attribute(nameAttribut,n[1].data[nameAttribut])
+                        this.listAttribute.push(att);
+                    }
+                }
+                if(!this.found(this.graph.nodes,n[0].metadata.id)){
                 this.graph.nodes.push(new NVNode(
                     new Branch(
-                        n[3].data.name,
-                        n[3].data.color,
-                        n[3].metadata.id),
-                    n[1].metadata.id,
-                    n[1].data.name,
+                        n[1].data.name,
+                        n[1].data.color,
+                        n[1].metadata.id),
+                    n[0].metadata.id,
+                    n[0].data.name,
                     this.listAttribute
                     )
                 );
             }
         });   
+
          if(this.user.preferedView == 1){
-             response.forEach(n => { // par chaque noeud
+             responseNode.forEach(n => { // par chaque noeud
                     this.listAttribute = new Array<Attribute>();
-                    console.log(n);
                     n[0].forEach(nameAttribut => {
                         if(nameAttribut != "name")
                             {
                                 var node  =
                                  new NVNode(new Branch(
-                            n[3].data.name,
-                            n[3].data.color,
-                            n[3].metadata.id), n[1].metadata.id+10,nameAttribut+" : "+n[1].data[nameAttribut],null,null,null, "attribut");
+                            n[2].data.name,
+                            n[2].data.color,
+                            n[2].metadata.id), n[1].metadata.id+10,nameAttribut+" : "+n[1].data[nameAttribut],null,null,null, "attribut");
                             this.graph.nodes.push(node);
                             this.graph.edges.push(new NVEdge(n[1].metadata.id*2,"",this.graph.nodes.find(x => x.id == n[1].metadata.id),node))
                             }
@@ -700,13 +689,12 @@ export class GraphUI {
             });
          }
         // hydratation des arcs
-        response.forEach(r => {
-            var source = this.found(this.graph.nodes,r[2].start.split("/")[r[2].start.split("/").length - 1]);
-            var target = this.found(this.graph.nodes,r[2].end.split("/")[r[2].end.split("/").length - 1]);
-            var edge = new NVEdge(r[2].metadata.id,r[2].data.name,source,target,r[2].metadata.type);
+        responseEdge.forEach(r => {
+            var source = this.found(this.graph.nodes,r[0].start.split("/")[r[0].start.split("/").length - 1]);
+            var target = this.found(this.graph.nodes,r[0].end.split("/")[r[0].end.split("/").length - 1]);
+            var edge = new NVEdge(r[0].metadata.id,r[0].data.name,source,target,r[0].metadata.type);
             this.graph.edges.push(edge);
-        }); 
-        console.log(this.graph.edges,this.graph.nodes);
+        });
     }
 }
 bootstrap(GraphUI);
